69b951a2d27621e90122c358018c9bf89d7bde27
lenovo <1461078880@qq.com>
2019-02-28 11:27:57 +0800

    动工

--- information/src/main/java/com/wd/information/InformationFragment.java
+++ information/src/main/java/com/wd/information/InformationFragment.java
@@ -1,9 +1,19 @@
 package com.wd.information;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.stx.xhb.xbanner.XBanner;
 import com.wd.common.baseFragment.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * @author : FangShiKang
@@ -14,6 +24,17 @@ import com.wd.common.baseFragment.BaseFragment;
 
 @Route(path = "/information/InformationFragment")
 public class InformationFragment extends BaseFragment {
+    @BindView(R2.id.message_classification)
+    Button mMessageClassification;
+    @BindView(R2.id.message_search)
+    Button mMessageSearch;
+    @BindView(R2.id.message_xbanner)
+    XBanner mMessageXbanner;
+    @BindView(R2.id.message_xrecy)
+    XRecyclerView mMessageXrecy;
+    private View view;
+    private Unbinder unbinder;
+
     @Override
     public int getLayoutId() {
         return R.layout.fragment_information;
@@ -37,5 +58,17 @@ public class InformationFragment extends BaseFragment {
     @Override
     protected void netFailed(Object s) {
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        view = View.inflate(getActivity(), R.layout.fragment_information, null);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
